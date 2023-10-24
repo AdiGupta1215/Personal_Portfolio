46f2c8b4575ab79c6285d7b7d4a41ae82347efd2
AdiGupta1215 <110678320+AdiGupta1215@users.noreply.github.com>
2023-10-23 23:28:58 -0400

    HTML/CSS changes

---
+++ script.js
@@ -0,0 +1,11 @@
+/* let homeElement = document.getElementById("header_home")
+
+function changeHeader(){
+    homeElement.innerText = "Random shit"
+}
+
+
+let testbtn = document.getElementById("testbutton")
+
+testbtn.addEventListener("click", changeHeader)
+*/
